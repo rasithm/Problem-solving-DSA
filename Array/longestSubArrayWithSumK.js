6c3727835ee7e4203c89867fe52fc4362dda1ba7
rasithm <mohamedrasith134@gmail.com>
2026-09-20 11:14:12 +0530

    day-6

---
+++ Array/longestSubArrayWithSumK.js
@@ -0,0 +1,44 @@
+//brute force approach
+const arr = [1, 2, 3, 1, 1, 1, 1, 4, 2, 3];
+const k = 3;
+let sum = 0;
+let maxlength = 0;
+for(let i = 0 ; i < arr.length ; i++){
+    sum = 0;
+    for(let j = i ; j < arr.length ; j++){
+        
+        sum = sum + arr[k]
+        if(sum === k){
+            maxlength = Math.max(maxlength , j - i + 1)
+        }
+    }
+}
+console.log(maxlength)
+
+
+//better approach using hashmap
+let currentsum = 0;
+let maxLength = 0;
+const map = new Map();
+for(let i = 0 ; i < arr.length ; i++){
+   
+    currentsum = currentsum + arr[i]
+    console.log(currentsum)
+
+    if(currentsum === k){
+        maxLength = i + 1
+    }
+
+    if(map.has(currentsum - k)){
+        
+        maxLength = Math.max(maxLength ,i - map.get(currentsum - k))
+        
+    }
+
+    if(!map.has(currentsum)){
+        
+        map.set(currentsum , i)
+    }
+
+}
+console.log(maxLength)
